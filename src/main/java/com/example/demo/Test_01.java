401118c7d77fd6d70c69fcfe4292aad228bf348e
hp-web <471513534@qq.com>
2019-12-21 23:35:23 +0800

    demo

---
+++ src/main/java/com/example/demo/Test_01.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test_01 {
+    @Test
+    public void TestCase(){
+        System.setProperty("webdriver.chrome.driver",getClass().getResource("/chromedriver.exe").getPath());
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.baidu.com");
+        
+
+
+    }
+
+}
