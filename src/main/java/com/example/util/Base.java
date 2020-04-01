fce304ed8d791194ff007627dbe26441507d9753
hp-web <56919405+hp-web@users.noreply.github.com>
2020-04-01 09:31:45 +0800

    Update Base.java

--- src/main/java/com/example/util/Base.java
+++ src/main/java/com/example/util/Base.java
@@ -16,5 +16,62 @@ public class Base {
         pw.print(text);
         pw.close();
     }
+    
+    
+        @Test
+    public void writeTxtFile() throws IOException {
+        //创建StringBuffer
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0;i<100;i++){
+            stringBuffer.append("Hello Java world, I am the exception.\n");
+
+        }
+        //System.out.println(stringBuffer.toString());
+        //写入txt
+        FileOutputStream fos = null;
+        OutputStreamWriter osw = null;
+        try{
+            fos = new FileOutputStream(new File("D:/test.txt"));
+            osw = new OutputStreamWriter(fos,"UTF-8");
+            osw.write(stringBuffer.toString());
+            osw.flush();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (fos!=null){
+                fos.close();
+            }if (osw!=null){
+                osw.close();
+            }
+        }
+        //读取txt
+        FileInputStream fis = null;
+        InputStreamReader isr= null;
+        BufferedReader br = null;
+        try {
+            fis = new FileInputStream(new File("D:/test.txt"));
+            isr = new InputStreamReader(fis,"UTF-8");
+            br = new BufferedReader(isr);
+            String str = "";
+            while ((str = br.readLine()) != null){
+                System.out.println(str);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (fis!=null){
+                fis.close();
+            }if (isr!=null){
+                isr.close();
+            }if (br!=null){
+                br.close();
+            }
+        }
+
+
+        
+        
+    }
 
 }
